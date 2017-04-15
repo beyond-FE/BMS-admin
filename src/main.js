19d69aa97af64fef1308e96aa86a8a02e1cf2153
qix <491781813@qq.com>
2017-04-15 15:25:11 +0800

    基于eslint规范修改部分代码

--- src/main.js
+++ src/main.js
@@ -1,57 +1,58 @@
-import babelPolyfill from "babel-polyfill"
-import Vue from 'vue'
-import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-import VueResource from 'vue-resource'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import routes from './routes'
+import babelPolyfill from 'babel-polyfill';
+import Vue from 'vue';
+import App from './App';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+// import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router';
+import store from './vuex/store';
+import Vuex from 'vuex';
+import VueResource from 'vue-resource';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import routes from './routes';
 
-import Mock from './mock'
-Mock.bootstrap();
+// import Mock from './mock'
+// Mock.bootstrap();
 
+import 'font-awesome/css/font-awesome.min.css';
 
-import 'font-awesome/css/font-awesome.min.css'
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+Vue.use(Vuex);
+Vue.use(VueResource);
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
-Vue.use(VueResource)
-
-NProgress.configure({ showSpinner: false });
+NProgress.configure({showSpinner: false});
 
 const router = new VueRouter({
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
   NProgress.start();
-  if (to.path == '/login') {
+  if (to.path === '/login') {
     sessionStorage.removeItem('user');
   }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
+  const user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path !== '/login') {
+    next({path: '/login'});
   } else {
-    next()
+    next();
   }
   next();
 });
 
 router.afterEach(transition => {
-NProgress.done();
+  NProgress.done();
 });
 
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
+  // el: '#app',
+  // template: '<App/>',
   router,
   store,
-  //components: { App }
-  render: h => h(App)
-}).$mount('#app')
+  // components: { App }
+  render: h => {
+    return h(App);
+  }
+}).$mount('#app');
--- src/pages/nav2/Page4.vue
+++ src/pages/nav2/Page4.vue
@@ -8,26 +8,25 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import { mapActions } from 'vuex'
+import {mapGetters, mapActions} from 'vuex';
 
-  export default {
-    computed: {
-  	// 使用对象展开运算符将 getters 混入 computed 对象中
+export default {
+  computed: {
+    // 使用对象展开运算符将 getters 混入 computed 对象中
     ...mapGetters([
       'getCount'
       // ...
     ])
-    },
-    methods: {
+  },
+  methods: {
     ...mapActions([
       'increment', // 映射 this.increment() 为 this.$store.dispatch('increment')
       'decrement'
     ])
-      //...mapActions({
-      //  add: 'increment' // 映射 this.add() 为 this.$store.dispatch('increment')
-      //})
-    }
+    // ...mapActions({
+    // add: 'increment' // 映射 this.add() 为 this.$store.dispatch('increment')
+    // })
   }
+};
 
 </script>
